0b5ccd1dbde12e44ec5407a9b3c8fcb6f4caadbc
Tony Agudo <antoniusmisfit@gmail.com>
2024-08-11 14:30:57 -0400

    Update

---
+++ unixbox.h
@@ -0,0 +1,16 @@
+//unixbox.h - Main header file
+//Written by Jose Anthony Agudo
+
+#include <stdio.h>
+#include <conio.h>
+#include <dirent.h>
+#include <dos.h>
+#include <errno.h>
+#include <pc.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <time.h>
